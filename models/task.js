1c581db37d9a9cdf31eeeeb72bee06fb4dff1a32
David Brown <gatech55@gmail.com>
2021-02-04 22:26:46 -0500

    added database information as well as installed antd and react-router-dom on the client and then installed mongoose and morgan on the backend package.json. last i added a seed file and seed line to the package.json

---
+++ models/task.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  target_behavior: {
+    type: String,
+    required: true,
+  },
+  token_value: {
+    type: Number,
+    required: true,
+  },
+  clinician_notes: {
+    type: String,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
