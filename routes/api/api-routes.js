cf1c4dbe51832295c5477b0bc2ceffefee36edfe
Hayes11 <bhayes81@gmail.com>
2021-02-05 21:20:36 -0500

    changed server.js with api route, created api routes for rewards db, added index.js file

---
+++ routes/api/api-routes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const db = require("../../models");
+
+// router.get("/api/workouts", (req, res) => {
+//     db.Workout.find()
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
+router.get("/api/healthcheck", (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+// router.route("/api/user/:id")
+//   .get(plantsController.findUserById);
+
+//health check route
+router.get("/api/rewards", (req, res) => {
+    db.Reward.find({}).then((foundRewards) => {
+        res.json(foundRewards);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.post("/api/rewards", (req, res) => {
+    db.Reward.create(req.body).then((newReward) => {
+        res.json(newReward);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/rewards/:id", ({ body, params }, res) => {
+    db.Reward.findByIdAndUpdate(
+        params.id,
+        { description: body },
+        // "runValidators" will ensure new exercises meet our schema requirements
+        { new: true, runValidators: true }
+    )
+        .then((updateReward) => {
+            res.json(updateReward);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// router.get("/api/workouts/range", (req, res) => {
+//     db.Workout.find({}).limit(7).sort({ date: -1 })
+//         .then((dbWorkout) => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+console.log("top");
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("client/build"));
-
+console.log("first");
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/Token-e-Economy",
   {
@@ -26,17 +24,23 @@ const connection = mongoose.connection;
 connection.on("connected", () => {
   console.log("Mongoose successfully connected!");
 });
-
+console.log("second");
 connection.on("error", (err) => {
   console.log("Mongoose connection error: ", err);
 });
 
+// const ProductsController = require("./controllers/productsController");
+
+app.use(express.static("client/build"));
+
 app.get("/api/config", (req, res) => {
-  res.json({
-    success: true,
-  });
+  res.json({ success: true });
 });
 
+// app.use("/api/products", ProductsController);
+//api routes
+app.use(require("./routes/api/api-routes"));
+console.log("third");
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
